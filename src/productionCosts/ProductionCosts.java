dfae4a0987d3119ddcc66a80c873b23333f13da5
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-07 22:42:41 +0300

    Initial commit

---
+++ src/productionCosts/ProductionCosts.java
@@ -0,0 +1,11 @@
+package productionCosts;
+
+public class ProductionCosts {
+
+    public float materialCost;
+    public float energyCost;
+    public float developmentCosts;
+    public float equipmentCosts;
+    public float salary;
+    public float businessExpenses;
+}
